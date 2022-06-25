21d632c3b2d73093486a58c335dcf9e9ba7ac360
fliker2309 <evgeniikapchik@gmail.com>
2022-06-25 22:45:33 +0300

    again doesn't work navigation

--- app/src/main/java/com/example/kittygram/presentation/ui/favorite/FavoriteFragment.kt
+++ app/src/main/java/com/example/kittygram/presentation/ui/favorite/FavoriteFragment.kt
@@ -1,4 +1,4 @@
-package com.example.kittygram.ui.notifications
+package com.example.kittygram.presentation.ui.favorite
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.kittygram.databinding.FragmentNotificationsBinding
+import com.example.kittygram.databinding.FragmentFavoriteBinding
 
-class NotificationsFragment : Fragment() {
+class FavoriteFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentFavoriteBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val favoriteViewModel =
+            ViewModelProvider(this).get(FavoriteViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textFavorite
+        favoriteViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/kittygram/presentation/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/example/kittygram/presentation/ui/favorite/FavoriteViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.kittygram.ui.home
+package com.example.kittygram.presentation.ui.favorite
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class FavoriteViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
