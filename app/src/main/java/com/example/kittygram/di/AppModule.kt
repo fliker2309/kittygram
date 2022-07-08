cdecb90b8504b4f012b9f49c1c9a27a028e4527c
fliker2309 <evgeniikapchik@gmail.com>
2022-07-08 17:18:34 +0300

    create RetrofitConfig.kt delete Provides from AppModule.kt

--- app/src/main/java/com/example/kittygram/di/AppModule.kt
+++ app/src/main/java/com/example/kittygram/di/AppModule.kt
@@ -3,10 +3,7 @@ package com.example.kittygram.di
 import com.example.kittygram.data.network.AuthInterceptor
 import com.example.kittygram.data.network.CatsService
 import com.example.kittygram.domain.repository.CatRepository
-import com.example.kittygram.domain.usecase.CatsUseCases
-import com.example.kittygram.domain.usecase.GetCatById
-import com.example.kittygram.domain.usecase.GetCats
-import com.example.kittygram.domain.usecase.RemoveCat
+import com.example.kittygram.domain.usecase.*
 import com.example.kittygram.utils.Constants.Companion.BASE_URL
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
@@ -23,47 +20,16 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
-
-    @Provides
-    fun baseUrl() = BASE_URL
-
-    @Provides
-    fun loggingInterceptor() = HttpLoggingInterceptor()
-        .setLevel(HttpLoggingInterceptor.Level.BODY)
-
-    @Provides
-    fun okHttpClient() = OkHttpClient.Builder()
-        .addInterceptor(loggingInterceptor())
-        .addInterceptor(AuthInterceptor())
-        .build()
-
-    @Provides
-    fun contentType() = "application/json".toMediaType()
-
-    @Provides
-    fun json() = Json {
-        prettyPrint = true
-        ignoreUnknownKeys = true
-    }
-
+ //todo запровайдить репозиторий и получить используемые им интерфейсы
+    
     @Provides
     @Singleton
-    fun provideRetrofit(baseUrl: String): CatsService =
-        Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .client(okHttpClient())
-            .addConverterFactory(json().asConverterFactory(contentType()))
-            .build()
-            .create(CatsService::class.java)
-
-    @Provides
-    @Singleton
-    fun provideCatsUseCases(repository: CatRepository) : CatsUseCases{
+    fun provideCatsUseCases(repository: CatRepository): CatsUseCases {
         return CatsUseCases(
             getCats = GetCats(repository),
             getCatById = GetCatById(repository),
             removeCatFromFavorite = RemoveCat(repository),
-            //todo add more use cases
+            saveCatToFavorite = SaveCat(repository)
         )
     }
 }
