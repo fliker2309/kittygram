41a18d3ebbb49effcf1b3400b601cadbabf2eb22
fliker2309 <evgeniikapchik@gmail.com>
2022-07-08 17:52:34 +0300

    rework CatDatabase.kt

--- app/src/main/java/com/example/kittygram/data/db/CatDatabase.kt
+++ app/src/main/java/com/example/kittygram/data/db/CatDatabase.kt
@@ -1,26 +1,18 @@
 package com.example.kittygram.data.db
 
-import android.content.Context
-import androidx.room.Room
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.kittygram.domain.model.Cat
+
+@Database(
+    entities = [Cat::class],
+    version = 1
+)
 
 abstract class CatDatabase : RoomDatabase() {
     abstract fun catDao(): CatDao
 
     companion object {
-        @Volatile
-        private var instance: CatDatabase? = null
-        private val LOCK = Any()
-        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
-            instance ?: buildDatabase(context).also { instance = it }
-        }
-
-        private fun buildDatabase(context: Context) =
-            Room.databaseBuilder(
-                context.applicationContext,
-                CatDatabase::class.java,
-                "cats_database"
-            )
-                .build()
+        const val DATABASE_NAME = "cats_db"
     }
 }
