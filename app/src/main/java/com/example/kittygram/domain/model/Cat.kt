37fdb1bedc32efd5f2c479a8eca4ee6f049023b9
fliker2309 <evgeniikapchik@gmail.com>
2022-07-08 17:23:59 +0300

    add columnInfo into Cat model

--- app/src/main/java/com/example/kittygram/domain/model/Cat.kt
+++ app/src/main/java/com/example/kittygram/domain/model/Cat.kt
@@ -1,12 +1,15 @@
 package com.example.kittygram.domain.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "cats")
 data class Cat(
     @PrimaryKey
+    @ColumnInfo(name = "Id")
     val id: String,
+    @ColumnInfo(name = "ImageUrl")
     val url: String
 ) {
     var isLiked: Boolean = false
